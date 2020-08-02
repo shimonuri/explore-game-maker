1aeca2ec2691dd7371d3e5f347e9fd261947ab04
Uri Shimon <uri.shimon5@gmail.com>
2020-08-02 09:00:44 +0000

    added console.log

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const gameEngine = new GameEngine();
  * Run a single loop of the game.
  * @param {object}   data    the game state.
  */
-function mainLoop(data) {}
+function mainLoop(data) {
+  console.log("hello");
+}
 
 gameEngine.startMainLoop(mainLoop, {});
